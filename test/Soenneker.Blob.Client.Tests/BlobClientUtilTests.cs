73c52310c75af24c0176ef74889d8735bb374b47
Jake Soenneker <jake@soenneker.com>
2024-12-20 10:22:19 -0600

    Tests

---
+++ test/Soenneker.Blob.Client.Tests/BlobClientUtilTests.cs
@@ -0,0 +1,25 @@
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Soenneker.Blob.Client.Abstract;
+using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+
+namespace Soenneker.Blob.Client.Tests;
+
+[Collection("Collection")]
+public class BlobClientUtilTests : FixturedUnitTest
+{
+    private readonly IBlobClientUtil _util;
+
+    public BlobClientUtilTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+        _util = Resolve<IBlobClientUtil>(true);
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
